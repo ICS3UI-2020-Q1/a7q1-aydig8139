e82c5442eba33a47c31fb4745ea0bca198f3f421
aydig8139 <aydig8139@wrdsb.ca>
2020-10-13 17:24:34 +0000

    finished exam grade question

--- Main.java
+++ Main.java
@@ -1,15 +1,39 @@
+import java.util.Scanner;
+
 /**
- *
- * @author 
+ * calculates letter grade
+ * @author Rose
  */
 public class Main {
+
+  //calculates the letter grade of a percentage given
+  public static void examGrade(double grade){
+    if (grade >= 80){
+      System.out.println("Your letter grade is A");
+    }else if (grade >= 70 && grade <= 79){
+      System.out.println("Your letter grade is B");
+    }else if (grade >= 60 && grade <= 69){
+      System.out.println("Your letter grade is C");
+    }else if (grade >= 50 && grade <= 59){
+      System.out.println("Your letter grade is D");
+    }else{
+      System.out.println("Your letter grade is F");
+    }
+    
+  }
 
   /**
    * The method that is executed when you hit the run button.
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // create new scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    System.out.println("What is your exam percentage?");
+    double grade = input.nextDouble();
+    examGrade(grade); 
     
+  
   }
 }
